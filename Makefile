7f3f13f2dbc2c7029946fc1f776a1049dcddb997
Goodluck Ubuntu@Oppo <goodluckogbuehi@gmail.com>
2024-02-04 04:01:48 +0100

    Added inline assembly for ARM32 and ARM64

--- Makefile
+++ Makefile
@@ -7,30 +7,30 @@ all: amd64 x86 arm aarch64 mips mips64 riscv64
 
 amd64: x86_64
 x86_64: 
-	x86_64-linux-gnu-gcc-13 -g main.c -o amd64.prog
+	x86_64-linux-gnu-gcc-13 -Og -g main.c -o amd64.prog
 
 i686: i386
 i386: x86
 x86:
-	i686-linux-gnu-gcc-13 -g main.c -o x86.prog
+	i686-linux-gnu-gcc-13 -Og -g main.c -o x86.prog
 
 arm64: aarch64
 aarch64:
-	aarch64-linux-gnu-gcc-13 -g main.c -o arm64.prog
+	aarch64-linux-gnu-gcc-13 -Og -g main.c -o arm64.prog
 
 arm32: arm
 arm:
-	arm-linux-gnueabi-gcc-13 -g main.c -o arm32.prog
+	arm-linux-gnueabi-gcc-13 -mcpu=cortex-a15 -mthumb -Og -g main.c -o arm32.prog
 
 mips: mips32
 mips32:
-	mips-linux-gnu-gcc-12 -g main.c -o mips32.prog
+	mips-linux-gnu-gcc-12 -Og -g main.c -o mips32.prog
 
 mips64:
-	mips64-linux-gnuabi64-gcc-12 -g main.c -o mips64.prog
+	mips64-linux-gnuabi64-gcc-12 -Og -g main.c -o mips64.prog
 
 riscv64:
-	riscv64-linux-gnu-gcc-13 -g main.c -o riscv64.prog
+	riscv64-linux-gnu-gcc-13 -Og -g main.c -o riscv64.prog
 
 clean:
 	rm -f ${wildcard *.prog}
--- main.c
+++ main.c
@@ -1,13 +1,40 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <sys/utsname.h>
+#include <limits.h>
 
+#if __WORDSIZE == 64
+#define FMT_HEX_64 "0x%lX"
+#else
+#define FMT_HEX_64 "0x%llX"
+#endif
 
 static void prologue(int argc, char* argv[]);
 static void epilogue(void);
+static uint64_t revbit64(uint64_t in);
+
+/* default implementation for revbit64_func */
+static uint64_t operation_default(uint64_t a, uint64_t b);
+
+/* {weak function to be overridden} */
+__attribute__ ((weak, alias("operation_default")))
+extern uint64_t operation_override(uint64_t a, uint64_t b);
+
+/* Function pointer, to be called */
+/* revbyte, revbit, add, return */
+static uint64_t (*const operation)(uint64_t a, uint64_t b) = operation_override;
 
 int main(int argc, char* argv[])
 {
 	prologue(argc, argv);
+
+	puts("\nMain tests... ");
+
+	uint64_t in = (0xf3Efull << 48) | 0x35dfull, out;
+	out = revbit64(in);
+	printf("revbit64 of " FMT_HEX_64 " is " FMT_HEX_64 "\n", in, out);
+
+	operation(5, 15);
 
 	epilogue();
 
@@ -32,6 +59,34 @@ static void prologue(int argc, char* argv[])
 
 static void epilogue(void)
 {
-	puts("Program done!");
+	puts("\nProgram done!");
+}
+
+__attribute__ ((always_inline))
+static inline uint64_t revbit64(uint64_t in)
+{
+	uint64_t output;
+
+#if defined(__aarch64__)
+	asm volatile ("rbit %0, %1" : "=r" (output) : "r" (in) );
+#elif defined(__arm__)
+	/* reverse input hi32 into output lo32 */
+	/* reverse input lo32 into output hi32 */
+	asm volatile ("rbit %0, %1" : "=r" (output) : "r" (in>>32) );
+	asm volatile ("rbit %0, %1" : "=r" (*(((uint32_t*)&output)+1)) : "r" (in) );
+#else
+	output = 0;
+	for(uint64_t i = 0; i < 64; i++)
+	{
+		if(in & (1ull << i))
+			output |= 1ull << (63 - i);
+	}
+#endif
+	return output;
+}
+
+static uint64_t operation_default(uint64_t a, uint64_t b)
+{
+	return a + b;
 }
 
